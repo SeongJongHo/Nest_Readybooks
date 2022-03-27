428959fe41d15e825df1c6739339ccdb7b372a33
SeongJongHo <whdgh9595@gmail.com>
2022-03-27 19:18:52 +0900

    [ADD]cart, book, user, order 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CartModule } from './cart/cart.module';
+import { BookModule } from './book/book.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, CartModule, BookModule, OrderModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
