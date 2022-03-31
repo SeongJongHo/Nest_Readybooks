765e8d405109dd0b7b4ec94bb37ab68373ff2b40
SeongJongHo <whdgh9595@gmail.com>
2022-03-31 19:06:26 +0900

    [MODIFY]main 주석추가

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  //포트 3000 서버 엶
 }
 bootstrap();
