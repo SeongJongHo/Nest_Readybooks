5c249b321ce56ab63005d2382bd03bedf8afed04
SeongJongHo <whdgh9595@gmail.com>
2022-04-03 10:40:36 +0900

    [MODIFY]주석제거

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,5 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  //포트 3000으로 서버엶
 }
 bootstrap();
