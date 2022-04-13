aee18515a0ca5221feb231b0bdb38d762e09ca56
SeongJongHo <whdgh9595@gmail.com>
2022-04-13 16:22:08 +0900

    [MODIFY]불필요한 주석 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,14 +13,14 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env'
     }),
     TypeOrmModule.forRoot({
-      type: 'mysql', //Database 설정
+      type: 'mysql', 
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: ['dist/**/*.entity.{ts,js}'], // Entity 연결
-      synchronize: false, //true 값을 설정하면 어플리케이션을 다시 실행할 때 엔티티안에서 수정된 컬럼의 길이 타입 변경값등을 해당 테이블을 Drop한 후 다시 생성해준다.
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: false, 
   })
   ],
   controllers: [],
