fabf1d85698ac918ffc7d861f2a8131c10f19c64
SeongJongHo <whdgh9595@gmail.com>
2022-04-14 23:59:02 +0900

    [MODIFY]user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,17 +9,26 @@ export class UserService {
     async signin(authorization: createUser['Authorization']): Promise<IUser>{
         const kakao: KakaoAPI= new KakaoAPI(authorization)
         const data: object= await kakao.get_kakao_user()
-        // const query: object= User
-        //     .createQueryBuilder('users')
-        //     .where('users.kakao_id= :kakao_id', data['id'])
-        // const user = await User.find({kakao_id:data['id']})
-        // console.log(user)
-        const result: IUser= {
+        const user = await User
+            .createQueryBuilder("user")
+            .where("user.kakao_id = :kakao_id", { kakao_id: data['id'] })
+            .getOne()
+            .then(result=>{
+                if(!result){
+                    
+                }
+            })
+            .catch(err=>{
+                err.message = 'DB_ERROR'
+                err.status = 400
+                throw new Error(err)
+            })
+        const userData: IUser= {
             Authorization: "test",
             user_nickname: "test",
             user_profile_img: "test",
             user_id: 123  
         }
-        return result
+        return userData
     }
 }
